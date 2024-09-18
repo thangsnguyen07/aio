d9bc79ec5ee9a019e2814493c5350ae1b359ed21
Thang Nguyen <thangsnguyen07@gmail.com>
2024-09-18 11:36:39 +0700

    fix(packages): core export

--- packages/core/src/shared/index.ts
+++ packages/core/src/shared/index.ts
@@ -3,4 +3,4 @@ export * from './domain/types.base'
 export * from './infrastructure/decorators/order.decorator'
 export * from './infrastructure/entities/base.entity'
 
-export * from './presentation/dtos/response.base.dto'
+export * from './presentation/dtos/response.dto'

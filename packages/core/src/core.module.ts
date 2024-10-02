bb3f9d02172f9339607475f331819c1fc3ff5e94
Thang Nguyen <thangsnguyen07@gmail.com>
2024-10-02 16:21:25 +0700

    refactor(core): update core module

--- packages/core/src/core.module.ts
+++ packages/core/src/core.module.ts
@@ -1,9 +1,42 @@
-import { Global, Module } from '@nestjs/common'
+import { DynamicModule, Module, ModuleMetadata, Provider } from '@nestjs/common'
 
-@Global()
-@Module({
-  imports: [],
-  providers: [],
-  exports: [],
-})
-export class CoreModule {}
+@Module({})
+export class CoreModule {
+  static register(options: CoreModuleOptions): DynamicModule {
+    return {
+      module: CoreModule,
+      providers: [
+        {
+          provide: 'CORE_MODULE_OPTIONS',
+          useValue: options,
+        },
+      ],
+    }
+  }
+
+  static registerAsync(options: CoreModuleAsyncOptions): DynamicModule {
+    return {
+      module: CoreModule,
+      imports: options.imports || [],
+      providers: [this.createAsyncOptionsProvider(options)],
+      exports: [this.createAsyncOptionsProvider(options)],
+    }
+  }
+
+  private static createAsyncOptionsProvider(options: CoreModuleAsyncOptions): Provider {
+    return {
+      provide: 'CORE_MODULE_OPTIONS',
+      useFactory: options.useFactory,
+      inject: options.inject || [],
+    }
+  }
+}
+
+export interface CoreModuleOptions {
+  grpcApiKey?: string
+}
+
+export interface CoreModuleAsyncOptions extends Pick<ModuleMetadata, 'imports'> {
+  useFactory?: (...args: any[]) => Promise<CoreModuleOptions> | CoreModuleOptions
+  inject?: any[]
+}
